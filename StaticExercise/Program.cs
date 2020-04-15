8c9a344ee44df6b29ee31a47d83270fd5421373d
brads83 <bradsorrells73@gmail.com>
2020-04-15 14:10:18 -0500

    finished

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -6,7 +6,19 @@ namespace StaticExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Please enter a temperature to convert to Fahrenheit");
+            //Console.ReadLine() = 
+            
+            Console.WriteLine($"Temp is {TempConverter.CelsiusToFahrenheit(45)}");
+
+            //Console.WriteLine("Please enter a temperature to convert to Celsius");
+            //Console.ReadLine() =
+            Console.WriteLine($"Celsius temp is {TempConverter.FahrenheitToCelsius(45)}");
         }
+
+        
+        
+
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double x)
+        {
+            double temp = (x - 32) * (5) / 9);
+            return temp;
+            //Console.WriteLine("(F - 32) x 5/9 = C");
+        }
+        public static double CelsiusToFahrenheit (double x)
+        {
+            double temp = (x * 9)/5 + 32;
+            return temp;
+            //Console.WriteLine("C x 9/5) + 32 = F");
+        }
+
+    }
+}
